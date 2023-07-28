4d80d253d1d37b8eb090fed9dc37018c2e9865ad
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-28 22:26:52 +0200

    Update web-browser.py

--- web-browser.py
+++ web-browser.py
@@ -1,2 +1,3 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QWidget, QDesktopWidget, QListWidget, QGridLayout, QProgressBar, QCompleter
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl, Qt
