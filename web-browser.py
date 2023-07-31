4fedabd358a96aa08782afc3dc4f8b6077396dfc
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-31 21:59:31 +0200

    Update web-browser.py

--- web-browser.py
+++ web-browser.py
@@ -202,3 +202,4 @@ class Browser(QMainWindow):
         self.restore_button.clicked.connect(self.showNormal)
         self.maximize_button = QPushButton(MAXIMIZE_BUTTON, self)
         self.maximize_button.setStyleSheet("""
+                QPushButton {
