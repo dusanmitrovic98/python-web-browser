2c1c8e956c666e8f85432156ca534e7db95e39cd
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-31 22:05:45 +0200

    Update web-browser.py

--- web-browser.py
+++ web-browser.py
@@ -223,3 +223,4 @@ class Browser(QMainWindow):
         self.maximize_button.clicked.connect(self.showMaximized)
         # self.maximize_button.clicked.connect(self.toggleFullscreen)
         self.close_button = QPushButton(CLOSE_BUTTON, self)
+        self.close_button.setStyleSheet("""
