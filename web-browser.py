d968583e53076c0e04dbb354e2d530e5accdacae
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-01 19:34:52 +0200

    Update web-browser.py

--- web-browser.py
+++ web-browser.py
@@ -326,3 +326,4 @@ class Browser(QMainWindow):
         # Set the layout for the main window
         central_widget = QWidget(self)
         central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
