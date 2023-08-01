22c81685b1c59dedcecc22ec89ab51a04ed32308
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-01 19:29:27 +0200

    Update web-browser.py

--- web-browser.py
+++ web-browser.py
@@ -315,3 +315,4 @@ class Browser(QMainWindow):
         nav_layout.addWidget(self.minimize_button)
         nav_layout.addWidget(self.restore_button)
         nav_layout.addWidget(self.maximize_button)
+        nav_layout.addWidget(self.close_button)
